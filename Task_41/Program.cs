a2415300f8b31d85e1907eaeffaf1c4be20accb5
Maksim <maksim6erbinin@mail.ru>
2022-07-14 00:06:25 +0300

    Added task 41

---
+++ Task_41/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите количество чисел: ");
+int count = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+for(int i = 1; i <= count; i++)
+{
+    Console.Write("Введите {0}-е число: ", i);
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    if(userNumber > 0) result++;    
+}
+
+Console.WriteLine("Чисел больше 0: {0} шт.", result);
