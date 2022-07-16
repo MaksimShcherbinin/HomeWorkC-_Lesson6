9eb9a3cdc58252f09b676d1bfd53bf6051b17f72
Maksim <maksim6erbinin@mail.ru>
2022-07-16 14:16:06 +0300

    function added in task 41

--- Task_41/Program.cs
+++ Task_41/Program.cs
@@ -1,12 +1,15 @@
-﻿Console.Write("Введите количество чисел: ");
-int count = Convert.ToInt32(Console.ReadLine());
-int result = 0;
-
-for(int i = 1; i <= count; i++)
+﻿int Filling (int count)
 {
-    Console.Write("Введите {0}-е число: ", i);
-    int userNumber = Convert.ToInt32(Console.ReadLine());
-    if(userNumber > 0) result++;    
+    int result = 0;
+    for(int i = 1; i <= count; i++)
+    {
+        Console.Write("Введите {0}-е число: ", i);
+        int userNumber = Convert.ToInt32(Console.ReadLine());
+        if(userNumber > 0) result++;    
+    }
+    return result;
 }
 
-Console.WriteLine("Чисел больше 0: {0} шт.", result);
+Console.Write("Введите количество чисел: ");
+int count = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Чисел больше 0: {0} шт.", Filling(count));
